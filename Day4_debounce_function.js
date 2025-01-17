e4e530d7319cf30ace9a05613316cd7b232364bb
Rahul Sharma <119799226+rahul87sharma@users.noreply.github.com>
2025-01-17 14:41:43 +0530

    Update and rename debounce_function.js to Day4_debounce_function.js
    name change

--- Day4_debounce_function.js
+++ Day4_debounce_function.js
@@ -1,3 +1,7 @@
+// Question : Debounce Function: Implement a debounce function that limits the frequency of a function's execution when called 
+//repeatedly within a specified time frame. This is useful in scenarios like resizing or scrolling events.
+
+
 function debounce(func, delay) {
     let timer;
     return function (...args) {
